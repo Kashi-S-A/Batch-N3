2f454bc7ee76181651ba59e0339e31dd953c8efb
Kashi-S-A <kashappaappannavar@gmail.com>
2025-09-03 19:17:48 +0530

    Insert operation

---
+++ JDBCProject/src/main/java/com/tyss/crud/Insert.java
@@ -0,0 +1,46 @@
+package com.tyss.crud;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Insert {
+
+	public static void main(String[] args) {
+
+		String url = "jdbc:postgresql://localhost:5432/company";
+		String username = "postgres";
+		String password = "root";
+
+		try {
+
+			// step 1 : Load the driver
+			Class.forName("org.postgresql.Driver");
+			System.out.println("Driver is loaded");
+
+			// step 2 : Create a connection
+			Connection con = DriverManager.getConnection(url, username, password);
+			System.out.println("connection is created");
+
+			// step 3 : Create a Statement
+			Statement stm = con.createStatement();
+			System.out.println("statement is created");
+
+			String sql = "insert into employee values(102,'Sandeep','sandeep@gmail.com',5000)";
+
+			// step 4 : Execute the query
+			stm.execute(sql);
+			System.out.println("query executed");
+
+			// step 5 : close the connection
+			con.close();
+			System.out.println("connection is closed");
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
