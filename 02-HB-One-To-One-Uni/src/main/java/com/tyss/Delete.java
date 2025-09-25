90a158e0fde7ccab94cc9c998e87b5c123a144da
Kashi-S-A <kashappaappannavar@gmail.com>
2025-09-25 19:18:58 +0530

    many to one

--- 02-HB-One-To-One-Uni/src/main/java/com/tyss/Delete.java
+++ 02-HB-One-To-One-Uni/src/main/java/com/tyss/Delete.java
@@ -1,5 +1,32 @@
 package com.tyss;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class Delete {
 
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("dev");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction et = em.getTransaction();
+
+		Engine engine = em.find(Engine.class, 222);
+
+		if (engine != null) {
+
+			et.begin();
+			// fetch car and setEngine as null
+			// update car
+			// remove engine
+			em.remove(engine);
+			et.commit();
+
+			System.out.println("engine deleted");
+		} else {
+			System.out.println("not found");
+		}
+
+	}
 }
---
+++ 04-HB-Many-To-One-Uni/src/main/java/com/ty/Book.java
@@ -0,0 +1,50 @@
+package com.ty;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Book {
+
+	@Id
+	private int id;
+	private String name;
+	private double price;
+
+	@ManyToOne
+	private Author author;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
+}
