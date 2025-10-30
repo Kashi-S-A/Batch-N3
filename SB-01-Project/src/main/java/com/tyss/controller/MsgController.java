11db550837475bbd12ecbf3e604d5903b18f3950
Kashi-S-A <kashappaappannavar@gmail.com>
2025-10-30 19:04:43 +0530

    save

--- SB-01-Project/src/main/java/com/tyss/controller/MsgController.java
+++ SB-01-Project/src/main/java/com/tyss/controller/MsgController.java
@@ -1,15 +1,26 @@
 package com.tyss.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class MsgController {
 
-	// apis
-	@RequestMapping("/msg")
-	public String getMessage() {
-		System.out.println("Message method triggered");
+	@RequestMapping(value = "/read", method = RequestMethod.POST)
+	public String readData(HttpServletRequest request) {
+		System.out.println("======User Data=========");
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String pwd = request.getParameter("pwd");
+
+		System.out.println(name);
+		System.out.println(email);
+		System.out.println(pwd);
 		return "message.jsp";
 	}
 
@@ -19,4 +30,26 @@ public class MsgController {
 		return "good.jsp";
 	}
 
+	@RequestMapping("/send")
+	public String sendData(Model model) {
+		String name = "John";
+		int age = 34;
+
+		model.addAttribute("username", name);
+		model.addAttribute("userage", age);
+
+		return "message.jsp";
+	}
+
+	@RequestMapping("/pass")
+	public ModelAndView sendData() {
+
+		Employee employee = new Employee(101, "Martin", "martin@gmail.com", 54000);
+
+		ModelAndView mv = new ModelAndView("greet.jsp");
+		mv.addObject("emp", employee);
+//		mv.setViewName("greet.jsp");
+
+		return mv;
+	}
 }
--- SB-01-Project/src/main/webapp/greet.jsp
+++ SB-01-Project/src/main/webapp/greet.jsp
@@ -7,6 +7,11 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<h1>This is Greet Jsp Page</h1>
+	<h1>Employee Details</h1>
+	<h3>Eid   : ${emp.id}</h3>
+	<h3>Name  : ${emp.name}</h3>
+	<h3>Email : ${emp.email}</h3>
+	<h3>Salary: ${emp.salary}</h3>
+	
 </body>
 </html>
