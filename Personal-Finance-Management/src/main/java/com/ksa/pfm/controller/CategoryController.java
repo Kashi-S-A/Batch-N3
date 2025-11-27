bdb008c499b0b9c93af1aa338e5be7a84b6e9993
Bhavik005 <bhavik@5.com>
2025-11-27 15:30:20 +0530

    Added 2 category files

---
+++ Personal-Finance-Management/src/main/java/com/ksa/pfm/controller/CategoryController.java
@@ -0,0 +1,59 @@
+package com.ksa.pfm.controller;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ksa.pfm.model.Category;
+import com.ksa.pfm.model.TransactionType;
+import com.ksa.pfm.model.User;
+import com.ksa.pfm.repo.UserRepo;
+import com.ksa.pfm.service.CategoryService;
+
+@Controller
+public class CategoryControlller {
+	
+	@Autowired
+	private UserRepo userRepo;
+	
+	@Autowired
+	private CategoryService categoryService;
+	
+	@GetMapping("/category")
+	public String categorypage(Principal principal, Model model)
+	{
+		User user = userRepo.findByEmail(principal.getName())
+				.orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+		
+		model.addAttribute("categories", categoryService.findByUser(user));
+		
+		return "category";
+	}
+	
+	@PostMapping("/category")
+	public String postCategory(Principal principal, @RequestParam String catName, @RequestParam TransactionType type, Model model)
+	{
+		User user = userRepo.findByEmail(principal.getName())
+				.orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+		
+		Category category = new Category();
+		category.setName(catName);
+		category.setType(type);
+		
+		category.setUser(user);
+		categoryService.saveCategory(category);
+						
+		List<Category> savedCategory = categoryService.findByUser(user);
+		
+		model.addAttribute("categories", savedCategory);
+		
+		return "category";
+	}
+}
